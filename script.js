d92f338c5e289881955ebd81f3f4cecb22fb8c07
Manko7 <35254355+Manko7@users.noreply.github.com>
2024-08-21 00:36:05 +0200

    Correcting finish - misunderstood challenge

--- script.js
+++ script.js
@@ -3,39 +3,31 @@ let canvas = document.getElementById("container")
 let rowCount = 16
 let rows = []
 let playerCount = 16
-let randomRGB = 0
-
-for (let i = 0; i < rowCount; i++) {
-    let row = document.createElement("div")
-    row.className = "rowCell"
-    canvas.appendChild(row)
-}
-
-rows = document.getElementsByClassName("rowCell")
+let randomRGB1 = 0
+let randomRGB2 = 0
+let randomRGB3 = 0
 
 
 function initiateGrid(playerCount) {
+
+    for (let i = 0; i < playerCount; i++) {
+        let row = document.createElement("div")
+        row.className = "rowCell"
+        canvas.appendChild(row)
+    }
+
+    rows = document.getElementsByClassName("rowCell")
+
     for (let index = 0; index < rows.length; index++) {
         for (let counter = 0; counter < playerCount; counter++) {
             let col = document.createElement("div")
             col.className = "colCell"
             col.onmouseover = function(e) {
-                switch(randomRGB = Math.floor(Math.random() * 3)) {
-                    case 0:
-                        this.classList.remove("paintBlue")
-                        this.classList.remove("paintGreen")
-                        this.classList.add("paintRed")
-                        break;
-                    case 1:
-                        this.classList.remove("paintBlue")
-                        this.classList.remove("paintRed")
-                        this.classList.add("paintGreen")
-                        break;
-                    case 2:
-                        this.classList.remove("paintGreen")
-                        this.classList.remove("paintRed")
-                        this.classList.add("paintBlue")
-                        break;
+                if (!this.style.backgroundColor) {
+                    randomRGB1 = Math.floor(Math.random() * 256)
+                    randomRGB2 = Math.floor(Math.random() * 256)
+                    randomRGB3 = Math.floor(Math.random() * 256)
+                    this.style.backgroundColor = "rgb(" + randomRGB1 + ", " + randomRGB2 + ", "+ randomRGB3 + ")"
                 }
             }
             rows[index].appendChild(col)
@@ -48,10 +40,7 @@ function setGrid() {
     while (newSize > 100) {
         newSize = window.prompt("New size exceeds 100, enter new value : ")
     }
-
-    for (let index = 0; index < rows.length; index++) {
-        rows[index].innerHTML = ""
-    }
+    canvas.innerHTML = ""
     initiateGrid(newSize);
 }
 
